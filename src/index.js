f18217760a677163aaf74ba958b6c20f297844ba
amarmedmoctar <amarmed4500@gmail.com>
2023-08-08 19:23:07 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
-import "font-awesome/css/font-awesome.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'font-awesome/css/font-awesome.css';
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
-ReactDOM.render(
+ReactDOM.render (
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById ('root')
 );
-registerServiceWorker();
+registerServiceWorker ();
